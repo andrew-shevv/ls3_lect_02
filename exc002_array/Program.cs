9d45d358b1c1a598e830a706753e48bdf2c38294
Andrew <vishnyaishere@gmail.com>
2022-08-19 18:22:03 +0300

    added exc002

---
+++ exc002_array/Program.cs
@@ -0,0 +1,13 @@
+﻿// max of 9 w/ array
+
+int[] array = {13, 6, 11, 43, 16, 27, 8, 54};
+
+int maxTotal = array[0];
+
+for (int i = 1; i < array.Length; i++) {
+    if (array[i] > maxTotal) {
+        maxTotal = array[i];
+    }
+}
+
+Console.WriteLine("maxTotal = " + maxTotal);
