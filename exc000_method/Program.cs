39f464a780662e086e2d5a5c4585010d91e47257
Andrew <vishnyaishere@gmail.com>
2022-08-19 17:39:24 +0300

    added exc000

---
+++ exc000_method/Program.cs
@@ -0,0 +1,23 @@
+﻿// max from 9 w/out method
+
+int n1 = 12;
+int n2 = 14;
+int n3 = 9;
+int n4 = 3;
+int n5 = 17;
+int n6 = 31;
+int n7 = 1;
+int n8 = 7;
+int n9 = 26;
+
+int max = n1;
+if (n2 > max) max = n2;
+if (n3 > max) max = n3;
+if (n4 > max) max = n4;
+if (n5 > max) max = n5;
+if (n6 > max) max = n6;
+if (n7 > max) max = n7;
+if (n8 > max) max = n8;
+if (n9 > max) max = n9;
+
+Console.WriteLine("max = " + max);
