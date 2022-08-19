b5fac8dadbb34008b9230402abb0c8ce1aa5823d
Andrew <vishnyaishere@gmail.com>
2022-08-19 17:57:06 +0300

    added exc001 two ways

--- exc001_method/Program.cs
+++ exc001_method/Program.cs
@@ -18,12 +18,18 @@ int Max(int arg1, int arg2, int arg3)
     return res;
 }
 
-int max1 = Max(a1, b1, c1);
-int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
-int maxTotal = Max(max1, max2, max3);
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// int maxTotal = Max(max1, max2, max3);
 
-Console.WriteLine("max1 = " + max1);
-Console.WriteLine("max2 = " + max2);
-Console.WriteLine("max3 = " + max3);
+int maxTotal = Max(
+    Max(a1, b1, c1), 
+    Max(a2, b2, c2), 
+    Max(a3, b3, c3)
+);
+
+// Console.WriteLine("max1 = " + max1);
+// Console.WriteLine("max2 = " + max2);
+// Console.WriteLine("max3 = " + max3);
 Console.WriteLine("maxTotal = " + maxTotal);
