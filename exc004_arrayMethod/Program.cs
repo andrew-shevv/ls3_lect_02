fb5b002bba72cc3cf97ecff170e5265b5a7055c5
Andrew <vishnyaishere@gmail.com>
2022-08-20 13:40:31 +0300

    exc004 optimised

--- exc004_arrayMethod/Program.cs
+++ exc004_arrayMethod/Program.cs
@@ -12,32 +12,27 @@ void PrintArray(int[] collection){
     }
 }
 
-string IndexOf(int[] collection, int find){
-    int position = 0;
-    bool check = false;
+int IndexOf(int[] collection, int find){
+    int position = -1;
     for (int i = 0; i < collection.Length; i++) {
         if (collection[i] == find) {
             position = i;
-            check = true;
             break; 
         }
     }
-
-    if (check == true) {
-        return "Element " + position + " of the array corresponds with number " + find;
-    }
-    else return "There is no such number in the array";
+    return position;
 }
 
 Console.WriteLine(" Input array length: ");
 int n = Convert.ToInt32(Console.ReadLine());
 int[] array = new int[n];
-
 FillArray(array);
 PrintArray(array);
 
 Console.WriteLine( "\n Input number to find element: ");
 int fnd = Convert.ToInt32(Console.ReadLine());
-string elem = IndexOf(array, fnd);
-
-Console.WriteLine(elem);
+int elem = IndexOf(array, fnd);
+if (elem != -1) {
+    Console.WriteLine("Element " + elem + " of the array corresponds with number " + fnd);
+}
+else Console.WriteLine("There is no such number in the array");
